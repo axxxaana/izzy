2fd7b431fc5a6066b1183cd346b8b18aab7e80bf
Axxxaana <contact@axaana.com>
2025-07-20 14:21:34 +0800

    Change Calendly embed height from 700px to 500px

--- src/pages/GetStartedPage.tsx
+++ src/pages/GetStartedPage.tsx
@@ -107,7 +107,7 @@ export const GetStartedPage: React.FC = () => {
               <div 
                 className="calendly-inline-widget" 
                 data-url="https://calendly.com/wildmountainimmigration/15min-call?hide_event_type_details=1&hide_gdpr_banner=1&primary_color=e44782" 
-                style={{ minWidth: '320px', height: '700px' }}
+                style={{ minWidth: '320px', height: '500px' }}
               ></div>
               
 
